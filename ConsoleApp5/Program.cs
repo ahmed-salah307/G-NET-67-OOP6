700c3b1d562a8d06bafb6d763dd95e57b1b9c799
Asalah <Asalah@MSI>
2026-09-03 14:39:09 +0300

    Q1

--- ConsoleApp5/Program.cs
+++ ConsoleApp5/Program.cs
@@ -4,7 +4,32 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region Q1
+            /*
+            Abstraction is about hiding complexity and essential background details 
+            while exposing only the necessary interface to the user
+
+
+Encapsulation is about bundling data and methods operating
+            on that data into a single unit(class) and restricting direct 
+            access to internal state using access modifiers like private or protected 
+
+            Real World Example
+
+            Atmospheric Weather Station
+
+            Pressing a "Get Temperature" button on a digital weather console You get the temperature display instantly
+            without needing to know how the thermistor calculates voltage drop => (Abstraction)
+
+            The inner circuit board, thermal sensors, and raw voltage data are sealed inside
+            a  weatherproof casing with private fields, You interact only through exposed public methods or properties like GetTemperatureInCelsius()
+             preventing external tampering with critical sensor calibration variables =>(Encapsulation)
+
+
+             */
+
+            #endregion
+
         }
     }
 }
